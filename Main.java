2099e66f2814bcc9ef92d7bb137002a369966291
Some558 <1641043@gmail.com>
2025-02-05 09:03:32 +0900

    スッキリjava6-1

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import comment.Zenhan;
 public class Main {
-  public static void main(String[] args) { 
-    System.out.println("Hello World");
+  public static void main(String[] args) throws Exception {
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    comment.Kouhan.callDeae();
+    comment.Kouhan.showMondokoro();
   }
 }
