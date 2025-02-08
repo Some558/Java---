96c719150243a3fb3c54bf45791755043e0dfdd1
Some558 <1641043@gmail.com>
2025-02-08 17:04:49 +0900

    練習11学習途中

--- 練習11/TangibleAsset.java
+++ 練習11/TangibleAsset.java
@@ -1,13 +1,13 @@
-public interface TangibleAsset {
-    String name;
-    int price;
+public abstract class TangibleAsset extends Asset implements Thing {
     String color;
-    public TangibleAsset(String name, int price, String color){
-        this.name = name;
-        this.price = price;
-        this.color = color;
-    };
-    public String getName(){ return this.name;}
-    public int getPrice(){ return this.price;}
-    public String getColor(){ return this.color;}
-}
+    double weight;
+    public TangibleAsset(String name, int price, String color, double weight) {
+      super(name, price);
+      this.color = color;
+      this.weight = weight;
+    }
+    public String getColor() { return this.color; }
+    public double getWeight() { return this.weight; }
+    public void setWeight(double weight) { this.weight = weight; }
+  }
+  
---
+++ 練習11/Thing.java
@@ -0,0 +1,8 @@
+public interface Thing {
+    public double getWeight(){
+        this.getWeight = getWeight;
+    }
+    public void setWeight(double setWeight){
+        this.setWeight = setWeight;
+    }
+}
