d6a1e20630b39995409e7574bf04c0f5eeadcdd0
Some558 <1641043@gmail.com>
2025-02-08 16:00:10 +0900

    11-2

---
+++ 練習11/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+    String isbn;
+    // コンストラクタ
+    public Book
+        (String name, int price, String color, String isbn){
+        super(name, price, color);
+        this.isbn = isbn;
+        }
+    // メソッド
+    public String getIsbn(){ return this.isbn;}
+}
+// ア Asset イ IntangibleAsset ウ Patent
---
+++ 練習11/Computer.java
@@ -0,0 +1,11 @@
+public class Computer implements TangibleAsset{
+    String makerName;
+    // コンストラクタ
+    public Computer
+        (String name, int price, String color, String makerName){
+        super(name, price, color);
+        this.makerName = makerName;
+        }
+    // メソッド
+    public String getMakerName(){ return this.makerName;}
+}
