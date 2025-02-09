4785108edaba5d27f3bb3fb4d9d12999952953f6
Some558 <1641043@gmail.com>
2025-02-09 12:46:56 +0900

    11-3

--- 練習11/TangibleAsset.java
+++ 練習11/TangibleAsset.java
@@ -1,8 +1,12 @@
-public abstract class TangibleAsset{
+public abstract class TangibleAsset extends Asset implements Thing{
     String color;
-    public TangibleAsset(String name, int price, String color){
+    double weight;
+    public TangibleAsset(String name, int price, String color, double weight){
     super(name, price);
     this.color = color;
+    this.weight = weight;
     }
     public String getColor(){ return this.color;}
+    public double getWeight(){ return this.weight;}
+    public void setWeight(double weight){this.weight = weight;}
 }
