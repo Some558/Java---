b5945affda21b9489a782b75e32dc51ef50352b6
Some558 <1641043@gmail.com>
2025-02-11 09:46:25 +0900

    12-3

--- 12/main.java
+++ 12/main.java
@@ -27,3 +27,15 @@ Hero h = (Hero) c;
 // Aを生成したが、ザックリYとみなして変数y1に代入する。
 // Bを生成したが、ザックリYとみなして変数y2に代入する。
 // AaBa
+
+// 12-3
+public class Main{
+    public static void main(String[] args){
+        Y[] array = new Y[2];
+        array[0] = new A();
+        array[1] = new B();
+        for(Y y ; array){
+            y.b();
+        }
+    }
+}
